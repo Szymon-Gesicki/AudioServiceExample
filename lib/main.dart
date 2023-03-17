e52d30103209aff3f1dfb1e4d4e476298bc9a968
Szymon Gesicki <szym.gesicki@gmail.com>
2023-03-17 11:34:57 +0100

    added audio service

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
+import 'package:audio_service/audio_service.dart';
 import 'package:audio_service_example/firebase_notification.dart';
 import 'package:audio_service_example/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+class AudioPlayerHandler extends BaseAudioHandler with SeekHandler {}
+
+late AudioHandler _audioHandler;
+
 void main() async {
+  _audioHandler = await AudioService.init(
+    builder: () => AudioPlayerHandler(),
+    config: const AudioServiceConfig(
+      androidNotificationChannelId: 'com.ryanheise.myapp.channel.audio',
+      androidNotificationChannelName: 'Audio playback',
+      androidNotificationOngoing: true,
+    ),
+  );
+
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(
